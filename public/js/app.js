af95ad21fb1f069c5db636b421a0366abb38636e
Erron <erronintila@gmail.com>
2021-05-31 14:54:15 +0800

    updated config

--- public/js/app.js
+++ public/js/app.js
@@ -2020,6 +2020,7 @@ try {
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.withCredentials = true;
+window.axios.defaults.baseURL = "http://127.0.0.1:8000/";
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,8 +22,8 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 window.axios.defaults.withCredentials = true;
+window.axios.defaults.baseURL = "http://127.0.0.1:8000/";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
