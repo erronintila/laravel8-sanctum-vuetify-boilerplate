0646208b8617caf157db4fc89061994829008b20
Erron <erronintila@gmail.com>
2021-05-31 14:46:36 +0800

    setup sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/v1/user', [AuthController::class, "user"]);
+
+Route::middleware('auth:sanctum')->prefix("v1")->group(function () {
+    Route::apiResources([
+        'users' => UserController::class
+    ]);
 });
